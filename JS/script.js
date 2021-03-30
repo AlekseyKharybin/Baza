38658f089a7be72ab01b3a6a27d48707907ad002
AlekseyKharybin <dante666games@yandex.ru>
2021-03-31 00:44:40 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,63 @@
+    //"use strict";
+    
+    
+    // let number = 5;
+    // const leftBorderWidth = 1;
+
+    // console.log(number);
+    // var name = "Ivan";
+
+    // {
+    //     let result = 50;
+    // }
+
+    //console.log(result);
+
+    // console.log(4/0);
+    // console.log(-4/0);
+    // console.log('string'/9);
+
+    // const persone = "5";
+    // console.log(persone);
+
+    // const obj = {
+    //     name: "John",
+    //     age: 25,
+    //     isMarried: false
+    // };
+
+    // //console.log(obj["age"]);
+
+    // let arr = ['plum.png', 6, 'orange.jpg', true];
+
+    // console.log(typeof arr.length);
+     
+    //alert()
+    //confirm
+    // const res = prompt('Дрочишь?','Естественно');
+    // console.log(typeof res);
+    // console.log(res + 5);
+
+    const answers = [];
+
+    // answers[0] = prompt('Имя','');
+    // answers[1] = prompt('Фимилия','');
+    // answers[2] = prompt('Возвраст','');
+
+    // console.log(typeof answers);
+    // console.log(typeof null);
+
+    // Интегполяция
+    const category = 'toys';
+    console.log(`https://someurl.com/${category}`);
+
+    const user = "Ivan";
+
+    // alert(`Привет ${user}.
+    // Как житуха?`);|
+
+    // alert('Привет ' + user + '\n' +
+    // 'Как житуха?');
+
+    //console.log(4 + +'4');
+    
